ae7a5c62c514d559b7984ed599a8ff8d80311cde
Rdx1565007 <uchchhwash@systechunimax.com>
2019-08-07 12:09:56 +0600

    changes made

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import {BrowserRouter as Router}  from 'react-router-dom';
 import ReactRouter from './router/router';
-
+import 'bootstrap/dist/css/bootstrap.css';
 
 
 
