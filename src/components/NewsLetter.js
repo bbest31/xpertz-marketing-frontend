01a2eb98c9af647cb98ad6593f182abd2e67daf7
nikultaka <nikultaka@gmail.com>
2019-08-12 14:27:26 +0530

    newsletter functionality completed

--- src/components/NewsLetter.js
+++ src/components/NewsLetter.js
@@ -1,16 +1,105 @@
 
-import React from 'react';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 import '../CSS/Header.css';
 import { Button } from 'react-bootstrap';
-import xSmall from '../asset/x-small.png'
+import { Container, Row, Col, Modal, Form } from 'react-bootstrap';
+import xSmall from '../asset/x-small.png';
+import Constant from '../constant.json';
+import $ from 'jquery';
 
 function NewsLetter() {
+    const [show, setShow] = useState(false);
+    const [show_error, setShowError] = useState(false);
+    const [show_success, setShowSuccess] = useState(false);
+    const [msg, setMsg] = useState('');
+    const handleClose = () => setShow(false);
+    const handleShow = () => {
+        setShow(true);
+        setShowError(false);
+        setShowSuccess(false);
+        setMsg('');
+    }
+    const validateEmail = (email) => {
+        var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+        return emailReg.test(email);
+    }
+    const submitnewsletter = (event) => {
+        const email = $("#email").val();
+        let count_error = 0;
+        if (email == '') {
+            $("#email").addClass('has_error');
+            count_error++;
+        } else {
+            if (!validateEmail(email)) {
+                $("#email").addClass('has_error');
+                count_error++;
+            } else {
+                $("#email").removeClass('has_error');
+            }
+        }
+
+        if (count_error == 0) {
+            const data = "email=" + email;
+            fetch(Constant.FIREBASE_URL + "/newsletter", {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/x-www-form-urlencoded',
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                body: data
+            })
+                .then(res => res.json())
+                .then(
+                    (result) => {
+                        if (result.status == '1') {
+                            setShowError(false);
+                            setShowSuccess(true);
+                            setMsg(result.msg);
+                        } else {
+                            setShowError(true);
+                            setShowSuccess(false);
+                            setMsg(result.msg);
+                        }
+                    },
+                )
+        }
+    }
     return (
-            <div>
-                <Button style={{backgroundColor:"white",color:"black",fontSize:42}} type="button" className="btn btn-secondary btn-lg">
+        <section>
+            <div onClick={handleShow}>
+                <Button style={{ backgroundColor: "white", color: "black", fontSize: 42 }} type="button" className="btn btn-secondary btn-lg">
                     <img src={xSmall} alt="Logo" height="48" /> Join Newsletter
                 </Button>
             </div>
+
+            <Modal show={show} onHide={handleClose}>
+                <div className="row">
+                    <div className="col-md-12 text-center">
+                        <div className={show_success ? "alert alert-success message_info show" : "alert alert-success message_info hidden"}>{msg}</div>
+                        <div className={show_error ? "alert alert-danger message_info show" : "alert alert-danger message_info hidden"}>{msg}</div>
+                    </div>
+                </div>
+                <Modal.Header closeButton>
+                    <Modal.Title>Newsletter</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" id="email" placeholder="Enter email" inputRef={(ref) => { this.email = ref }} required />
+                            <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                        </Form.Text>
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>Close</Button>
+                    <Button variant="primary" onClick={submitnewsletter}>Submit</Button>
+                </Modal.Footer>
+            </Modal>
+        </section>
     );
 }
 
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 import '../CSS/Dashboard.css';
-import { Container, Row, Col,Button } from 'react-bootstrap';
+import { Container, Row, Col, Button, Modal, Form } from 'react-bootstrap';
 import AllLevelLogo1 from '../asset/HR.png'
 import AllLevelLogo2 from '../asset/Symbol.png'
 import AllLevelLogo3 from '../asset/Frame.png'
@@ -11,110 +12,118 @@ import xsection3 from '../asset/xsection3.png'
 import xsection4 from '../asset/xsection4.png'
 import xsection from '../asset/X.png'
 import NewsLetterButton from '../components/NewsLetter'
+import { DashCarousel } from "../components/DashboardCarousel"
 
-import {DashCarousel} from "../components/DashboardCarousel"
 
 function Dashboard() {
   return (
-    <div className="dashboard" style={{padding:0}}>
-    <Container fluid={true} style={{padding:0}}>
-      <div>
-        <div className="head-text">
+    <div className="dashboard" style={{ padding: 0 }}>
+      <Container fluid={true} style={{ padding: 0 }}>
+        <div>
+          <div className="head-text">
             <p>LARGE SCALE COLLABORATION</p>
             <span>&</span>
             <p>IN-DEPTH SKILL MANAGEMENT</p>
-        <hr className="hr"></hr>
-        </div>
+            <hr className="hr"></hr>
+          </div>
 
-        <div className="text-center">
+          <div className="text-center">
             <p className="sub-head-text">PUBLIC EXPERTISE PROFILES FOR EMPLOYEES</p>
             <p className="sub-head-text">AND INSIGHTFUL ANALYTICS FOR MANAGEMENT</p>
-        </div>
-      <DashCarousel></DashCarousel>
-      <Row className="level">
-        <Col>
-        <p style={{fontSize:48,color:"#4F4F4F"}}>BENEFITS AT ALL LEVELS</p>
-        </Col>
-      </Row>
-      <Row style={{color:"black"}}>
-        <Col xs={12} sm={6} md={3} style={{backgroundColor:"white",paddingTop:2+"rem",fontSize:20}}>   
-          <img height="160" width="220" src={AllLevelLogo1} alt="" />
-          <p className="pt-3">HUMAN RESOURCES</p>
-        </Col>
-        <Col xs={12} sm={6} md={3} style={{backgroundColor:"white",paddingTop:2+"rem",fontSize:20}}>   
-          <img height="126" width="188" src={AllLevelLogo2}valt="" />
-          <p className="pt-3">PROJECT MANAGEMENT</p>
-        </Col>
-        <Col xs={12} sm={6} md={3} style={{backgroundColor:"white",paddingTop:2+"rem",fontSize:20}}>  
-          <img height="160" width="220" src={AllLevelLogo3} alt="" />
-          <p className="pt-3">UPPER MANAGEMENT</p>
-        </Col>
-        <Col xs={12} sm={6} md={3} style={{backgroundColor:"white",paddingTop:2+"rem",fontSize:20}}>   
-          <img height="156" width="174" src={AllLevelLogo4} alt="" />
-          <p className="pt-3">EVERYONE ELSE</p>
-        </Col>
-      </Row>
-      {/* <Row style={{color:"black"}}>
+          </div>
+          <DashCarousel></DashCarousel>
+          <Row className="level">
+            <Col>
+              <p style={{ fontSize: 48, color: "#4F4F4F" }}>BENEFITS AT ALL LEVELS</p>
+            </Col>
+          </Row>
+          <Row style={{ color: "black" }}>
+            <Col xs={12} sm={6} md={3} style={{ backgroundColor: "white", paddingTop: 2 + "rem", fontSize: 20 }}>
+              <img height="160" width="220" src={AllLevelLogo1} alt="" />
+              <p className="pt-3">HUMAN RESOURCES</p>
+            </Col>
+            <Col xs={12} sm={6} md={3} style={{ backgroundColor: "white", paddingTop: 2 + "rem", fontSize: 20 }}>
+              <img height="126" width="188" src={AllLevelLogo2} valt="" />
+              <p className="pt-3">PROJECT MANAGEMENT</p>
+            </Col>
+            <Col xs={12} sm={6} md={3} style={{ backgroundColor: "white", paddingTop: 2 + "rem", fontSize: 20 }}>
+              <img height="160" width="220" src={AllLevelLogo3} alt="" />
+              <p className="pt-3">UPPER MANAGEMENT</p>
+            </Col>
+            <Col xs={12} sm={6} md={3} style={{ backgroundColor: "white", paddingTop: 2 + "rem", fontSize: 20 }}>
+              <img height="156" width="174" src={AllLevelLogo4} alt="" />
+              <p className="pt-3">EVERYONE ELSE</p>
+            </Col>
+          </Row>
+          {/* <Row style={{color:"black"}}>
         <Col xs={3} style={{backgroundColor:"white",paddingTop:2+"rem"}}>  HUMAN RESOURCES</Col>
         <Col xs={3} style={{backgroundColor:"white",paddingTop:2+"rem"}}>  PROJECT MANAGEMENT</Col>
         <Col xs={3} style={{backgroundColor:"white",paddingTop:2+"rem"}}>  UPPER MANAGEMENT</Col>
         <Col xs={3} style={{backgroundColor:"white",paddingTop:2+"rem"}}>  EVERYONE ELSE</Col>
       </Row> */}
-      </div>
-      <hr className="full-hr"></hr>
+        </div>
+        <hr className="full-hr"></hr>
 
-      <div className="pt-4 pb-4" style={{backgroundColor:"white", color:"black",fontWeight:"bold",}}>
-        <div className="d-flex flex-row flex-wrap">
+        <div className="pt-4 pb-4" style={{ backgroundColor: "white", color: "black", fontWeight: "bold", }}>
+          <div className="d-flex flex-row flex-wrap">
             <div className="d-flex flex-column col-md-4 col-xs-12">
-                <div className="d-flex flex-row justify-content-center">
-                    <div className="x-section">
-                        <img src={xsection1} /> 
-                        <p className="pt-1">LARGE SCALE COLLABORATION</p>
-                        <p className="sub d-none d-sm-block">Search for colleagues by skill to find the right person to solve problems</p>
-                    </div>
+              <div className="d-flex flex-row justify-content-center">
+                <div className="x-section">
+                  <img src={xsection1} />
+                  <p className="pt-1">LARGE SCALE COLLABORATION</p>
+                  <p className="sub d-none d-sm-block">Search for colleagues by skill to find the right person to solve problems</p>
                 </div>
-                <div className="d-flex flex-row justify-content-center">
-                    <div className="x-section">
-                        <img src={xsection3} alt="Logo"/> 
-                        <p className="pt-1">SKILLED BASED TARGETED HIRING</p>
-                        <p className="sub d-none d-sm-block">See how your employees are performing and structural changes are having an impact</p>
-                    </div>
+              </div>
+              <div className="d-flex flex-row justify-content-center">
+                <div className="x-section">
+                  <img src={xsection3} alt="Logo" />
+                  <p className="pt-1">SKILLED BASED TARGETED HIRING</p>
+                  <p className="sub d-none d-sm-block">See how your employees are performing and structural changes are having an impact</p>
                 </div>
+              </div>
             </div>
             <div className="d-none flex-column col-4 justify-content-center d-md-flex">
-                <img className="d-none d-sm-block ml-auto mr-auto x-image" height="240" width="260" src={xsection} />
+              <img className="d-none d-sm-block ml-auto mr-auto x-image" height="240" width="260" src={xsection} />
             </div>
             <div className="d-flex flex-column col-md-4 col-xs-12">
 
-            <div className="d-flex flex-row justify-content-center">
-                    <div className="x-section">
-                        <img src={xsection2} height="192" width="229" />
-                        <p className="pt-1">ANALYTICS INTO COLLABORATION EFFORTS</p>
-                        <p className="sub d-none d-sm-block">quickly identify the skills in high demand for targeting in your recruitment</p>
-                    </div>
+              <div className="d-flex flex-row justify-content-center">
+                <div className="x-section">
+                  <img src={xsection2} height="192" width="229" />
+                  <p className="pt-1">ANALYTICS INTO COLLABORATION EFFORTS</p>
+                  <p className="sub d-none d-sm-block">quickly identify the skills in high demand for targeting in your recruitment</p>
                 </div>
-                <div className="d-flex flex-row justify-content-center">
-                    <div className="x-section">
-                        <img src={xsection4} alt="Logo" /> 
-                        <p className="pt-1">ACCURATE RESOURCE ALLOCATION</p>
-                        <p className="sub d-none d-sm-block">Concrete insights into the skills present within the company and the level of strength in order to delegate people effectively</p>
-                    </div>
+              </div>
+              <div className="d-flex flex-row justify-content-center">
+                <div className="x-section">
+                  <img src={xsection4} alt="Logo" />
+                  <p className="pt-1">ACCURATE RESOURCE ALLOCATION</p>
+                  <p className="sub d-none d-sm-block">Concrete insights into the skills present within the company and the level of strength in order to delegate people effectively</p>
                 </div>
+              </div>
             </div>
+          </div>
         </div>
-      </div>
-      <Row style={{color:"white",fontSize:48,fontWeight:"bold",paddingTop:4+"rem",paddingBottom:6+"rem"}}>
+        <Row style={{ color: "white", fontSize: 48, fontWeight: "bold", paddingTop: 4 + "rem", paddingBottom: 6 + "rem" }}>
           <Col xs={12} className="join-news-letter" >
-              <p className="pt-3">JOIN OUR NEWS LETTER FOR UPDATES</p>
-              <NewsLetterButton></NewsLetterButton>
+            <p className="pt-3">JOIN OUR NEWS LETTER FOR UPDATES</p>
+            <NewsLetterButton></NewsLetterButton>
           </Col>
-      </Row>
-      
+        </Row>
 
 
-    </Container>
+
+
+      </Container>
+
+
+
+
+
     </div>
   );
 }
 
 export default Dashboard;
+
+
